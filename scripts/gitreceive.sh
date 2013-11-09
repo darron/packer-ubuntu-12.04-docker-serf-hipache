d83c492da89126e7e56ae21524fb866d475c5215
Darron Froese <darron@froese.org>
2013-11-08 17:42:22 -0700

    final setup

--- scripts/gitreceive.sh
+++ scripts/gitreceive.sh
@@ -6,6 +6,12 @@ sudo wget https://raw.github.com/progrium/gitreceive/master/gitreceive
 sudo chmod +x gitreceive 
 sudo gitreceive init
 sudo echo "git ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/80-git
+cd /home/git/
+sudo wget https://raw.github.com/darron/packer-ubuntu-12.04-docker-serf-hipache/master/receiver.sh
+sudo mv -v receiver.sh receiver
+
+echo "Do this now: "
+echo 'cat ~/.ssh/id_dsa.pub | ssh ubuntu@server.goes.here -i key.pem "sudo gitreceive upload-key ubuntu"'
 
 # On the machine that will push git repos over:
 #
