fb2d8a3ca8e5f8ce6359518013fc9e83d02853a7
Darron Froese <darron@froese.org>
2013-11-09 19:07:37 -0700

    Handle the internal exposed port better - not just 5000 anymore.

--- receiver.sh
+++ receiver.sh
@@ -9,13 +9,16 @@ echo "Base: $BASE"
 OLD_ID=$(sudo docker ps | grep "$BASE:latest" | cut -d ' ' -f 1)
 OLD_PORT=$(sudo docker inspect $OLD_ID | grep "HostPort" | cut -d ':' -f 2 | cut -d '"' -f 2)
 
+# Look for the exposed port.
+INTERNAL_PORT=$(grep "EXPOSE" /home/git/src/$1/Dockerfile | cut -d ' ' -f 2)
+
 # Build and get the ID.
 sudo docker build -t nonfiction/$BASE /home/git/src/$1
 ID=$(sudo docker run -P -d nonfiction/$BASE)
 # Get the $PORT from the container.
-PORT=$(sudo docker port $ID 5000 | sed 's/0.0.0.0://')
+PORT=$(sudo docker port $ID $INTERNAL_PORT | sed 's/0.0.0.0://')
 
-# Zero out any existing config items.
+# Zero out any existing items.
 /usr/bin/redis-cli ltrim frontend:$BASE.handbill.io 200 200
 # Connect $BASE.handbill.io to the $PORT
 /usr/bin/redis-cli rpush frontend:$BASE.handbill.io $BASE
