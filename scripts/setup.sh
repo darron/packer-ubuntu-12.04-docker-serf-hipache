87fd17c54602de4a5ab7fa6b5c108934749f5da5
Darron Froese <darron@froese.org>
2013-11-09 19:09:32 -0700

    Adding locale setup.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,6 +5,8 @@ sudo apt-get update
 sudo apt-get install -y python-software-properties
 sudo apt-get -y upgrade
 
+/usr/sbin/update-locale
+
 # install the backported kernel
 sudo apt-get install -y linux-image-generic-lts-raring linux-headers-generic-lts-raring
 
